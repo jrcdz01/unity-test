f2b8b178f4dc2b6bb6f8764417ed84fea738741e
Alfredo Junior <jrcdz01@gmail.com>
2023-11-29 05:47:47 -0300

    Add cooldown on shoot and PlayerShooting

---
+++ Assets/Scripts/GeneralScripts/Shoot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] protected GameObject projectile;
+    [SerializeField] private string targetTag;
+    [SerializeField] private int damagePower;
+    [SerializeField] protected float cooldownTime;
+    private readonly CooldownTimer timer;
+
+    public void SingleShoot(Vector2 cannonPosition, Quaternion cannonRotation){
+        // timer.StartTimer(cooldownTime);
+        StartCoroutine(CannonShoot(cannonPosition, cannonRotation));
+    }
+
+    public void SideTripleShoot(Vector2 cannonPosition, Quaternion cannonRotation){
+        // timer.StartTimer(cooldownTime);
+        StartCoroutine(CannonShoot(cannonPosition, cannonRotation));
+        StartCoroutine(CannonShoot(cannonPosition, cannonRotation*Quaternion.Euler(0,0,30).normalized));
+        StartCoroutine(CannonShoot(cannonPosition, cannonRotation*Quaternion.Euler(0,0,-30).normalized));
+    }
+
+    protected IEnumerator CannonShoot( Vector2 cannonPosition, Quaternion rotation){
+        Projectile fire = Instantiate(projectile, cannonPosition, rotation).GetComponent<Projectile>();
+        
+        fire.Setup(targetTag, damagePower);
+        yield return null;
+    }
+}
--- Assets/Scripts/PlayerScripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerScripts/PlayerShooting.cs
@@ -5,60 +5,35 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Timeline;
 
-public class PlayerShoot : MonoBehaviour
+public class PlayerShooting : Shoot
 {
     [SerializeField] GameObject pivotFrontCannon;
     [SerializeField] GameObject pivotRightCannon;
     [SerializeField] GameObject pivotLeftCannon;
 
     [Header("Projectile Stuff")]
-    [SerializeField] protected GameObject projectile;
-    [SerializeField] private string targetTag;
-    [SerializeField] private int damagePower;
+   
     private Vector2 cannonSelected;
     private Quaternion cannonRotation;
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
+        if(Input.GetButtonDown("Fire1")  ){
             cannonSelected = pivotFrontCannon.transform.position;
             cannonRotation = transform.rotation;
-            StartCoroutine(CannonShoot(cannonSelected, cannonRotation));
+            SingleShoot(cannonSelected, cannonRotation);
         }
 
         if(Input.GetButtonDown("Fire2")){
             cannonSelected = pivotLeftCannon.transform.position;
             cannonRotation = transform.rotation*Quaternion.Euler(0,0,90).normalized;
-            
-            StartCoroutine(CannonShoot(cannonSelected, cannonRotation));
-            StartCoroutine(CannonShoot(cannonSelected, cannonRotation*Quaternion.Euler(0,0,30).normalized));
-            StartCoroutine(CannonShoot(cannonSelected, cannonRotation*Quaternion.Euler(0,0,-30).normalized));
+            SideTripleShoot(cannonSelected, cannonRotation);
         }
 
         if(Input.GetButtonDown("Fire3")){
             cannonSelected = pivotRightCannon.transform.position;
             cannonRotation = transform.rotation*Quaternion.Euler(0,0,-90).normalized;
-            
-            StartCoroutine(CannonShoot(cannonSelected, cannonRotation));
-            StartCoroutine(CannonShoot(cannonSelected, cannonRotation*Quaternion.Euler(0,0,10).normalized));
-            StartCoroutine(CannonShoot(cannonSelected, cannonRotation*Quaternion.Euler(0,0,-10).normalized));
+            SideTripleShoot(cannonSelected, cannonRotation);
         }
-    }
-
-    IEnumerator CannonShoot( Vector2 cannonPosition, Quaternion rotation ){
-        Projectile fire = Instantiate(projectile, cannonPosition, rotation).GetComponent<Projectile>();
-        
-        Vector3 direction = ChooseProjectileDirection(pivotFrontCannon);
-        
-        fire.Setup(targetTag, damagePower);
-        yield return null;
-    }
-
-
-    private Vector3 ChooseProjectileDirection(GameObject cannon){
-        float posY = cannon.transform.position.y;
-        float posX = cannon.transform.position.x;
-        float temp = Mathf.Atan2(posY, posX) * Mathf.Rad2Deg;
-        return new Vector3(0,0, temp);
-    }
+    }  
 }
